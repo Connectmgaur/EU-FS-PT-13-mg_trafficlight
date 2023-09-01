adcc87158d9702fd522b743cf47666f7239a49e8
Meenakshi Gaur <136475194+Connectmgaur@users.noreply.github.com>
2023-09-01 19:30:01 +0000

    Final Commit

--- src/js/component/trafficlight.jsx
+++ src/js/component/trafficlight.jsx
@@ -1,33 +1,33 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 
 const TrafficLight = (props) => {
   const [color, setColor] = useState("red");
 
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      if (color === "red") {
+        setColor("green");
+      } else if (color === "green") {
+        setColor("orange");
+      } else if (color === "orange") {
+        setColor("red");
+      }
+    }, 3000);
 
+    return () => clearTimeout(timer);
+  }, [color]);
 
 
   return (
     <div>  <h1>{props.test}</h1>
-   
-    <div className="traffic_light">
-      <div
-        onClick={() => setColor("red")}
-        className={"light red" + (color === "red" ? " glow" : "")}
-      ></div>
-
-      <div
-        onClick={() => setColor("orange")}
-        className={"light orange" + (color === "orange" ? " glow" : "")}
-      ></div>
-
-      <div
-        onClick={() => setColor("green")}
-        className={"light green" + (color === "green" ? " glow" : "")}
-      ></div>
+       <div className="traffic_light">
+      <div className={"light red" + (color === "red" ? " glow" : "")}></div>
+      <div className={"light orange" + (color === "orange" ? " glow" : "")}></div>
+      <div className={"light green" + (color === "green" ? " glow" : "")}></div>
     </div>
     </div>
   );
-  }
+};
 
 export default TrafficLight;
