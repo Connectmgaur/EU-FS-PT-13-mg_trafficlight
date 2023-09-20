9279f12cdf0686d22c13d0e0beb278c066a58362
Meenakshi Gaur <136475194+Connectmgaur@users.noreply.github.com>
2023-09-20 14:07:58 +0000

    Button added to change color

--- src/js/component/trafficlight.jsx
+++ src/js/component/trafficlight.jsx
@@ -1,33 +1,30 @@
 import React, { useState, useEffect } from "react";
 
 
-const TrafficLight = (props) => {
+const TrafficLight = () => {
   const [color, setColor] = useState("red");
 
-  useEffect(() => {
-    const timer = setTimeout(() => {
-      if (color === "red") {
-        setColor("green");
-      } else if (color === "green") {
-        setColor("orange");
-      } else if (color === "orange") {
-        setColor("red");
-      }
-    }, 3000);
-
-    return () => clearTimeout(timer);
-  }, [color]);
-
+  const handleColorChange = () => {
+    if (color === "red") {
+      setColor("green");
+    } else if (color === "green") {
+      setColor("orange");
+    } else if (color === "orange") {
+      setColor("red");
+    }
+  };
 
   return (
-    <div>  <h1>{props.test}</h1>
-       <div className="traffic_light">
-      <div className={"light red" + (color === "red" ? " glow" : "")}></div>
-      <div className={"light orange" + (color === "orange" ? " glow" : "")}></div>
-      <div className={"light green" + (color === "green" ? " glow" : "")}></div>
-    </div>
+    <div>
+      <div className="traffic_light">
+        <div className={"light red" + (color === "red" ? " glow" : "")}></div>
+        <div
+          className={"light orange" + (color === "orange" ? " glow" : "")}
+        ></div>
+        <div className={"light green" + (color === "green" ? " glow" : "")}></div>
+      </div>
+      <button className="btn" onClick={handleColorChange}>Change Color</button>
     </div>
   );
 };
-
 export default TrafficLight;
