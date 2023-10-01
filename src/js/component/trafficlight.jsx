b5ebf6c796a1c3ec4794cb6b8cbdf72f0658eb0d
Meenakshi Gaur <136475194+Connectmgaur@users.noreply.github.com>
2023-10-01 22:23:54 +0000

    As per requirement

--- src/js/component/trafficlight.jsx
+++ src/js/component/trafficlight.jsx
@@ -2,29 +2,46 @@ import React, { useState, useEffect } from "react";
 
 
 const TrafficLight = () => {
-  const [color, setColor] = useState("red");
+  const [activeColor, setActiveColor] = useState('red');
 
-  const handleColorChange = () => {
-    if (color === "red") {
-      setColor("green");
-    } else if (color === "green") {
-      setColor("orange");
-    } else if (color === "orange") {
-      setColor("red");
-    }
-  };
+ // const handleColorChange = () => {
+   // if (color === "red") {
+   //   setColor("green");
+    //} else if (color === "green") {
+     // setColor("orange");
+    //} else if (color === "orange") {
+     // setColor("red");
+    //}
+ // };
 
-  return (
-    <div>
-      <div className="traffic_light">
-        <div className={"light red" + (color === "red" ? " glow" : "")}></div>
-        <div
-          className={"light orange" + (color === "orange" ? " glow" : "")}
-        ></div>
-        <div className={"light green" + (color === "green" ? " glow" : "")}></div>
-      </div>
-      <button className="btn" onClick={handleColorChange}>Change Color</button>
-    </div>
-  );
+  const handleClick = (color) => {
+    setActiveColor(color);
 };
+
+const isLightSelected = (color) => {
+  return activeColor === color;
+};
+
+return (
+  <div>
+    <div className="traffic_light">
+      <div
+        className={`light red ${isLightSelected("red") ? "selected glow" : ""}`}
+        onClick={() => handleClick("red")}
+      ></div>
+      <div
+        className={`light orange ${
+          isLightSelected("orange") ? "selected glow" : ""
+        }`}
+        onClick={() => handleClick("orange")}
+      ></div>
+      <div
+        className={`light green ${isLightSelected("green") ? "selected glow" : ""}`}
+        onClick={() => handleClick("green")}
+      ></div>
+    </div>
+  </div>
+);
+};
+
 export default TrafficLight;
